7728ca6a325f3fc8d6c05636abac31edc907ef0e
Frank Cannon <cannonfl@gmail.com>
2018-03-28 19:03:07 -0700

    add output to webpack.config and related

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,12 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require('path');
 
 module.exports = {
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
   module: {
     rules: [
       {
